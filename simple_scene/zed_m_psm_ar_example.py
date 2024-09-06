9f0379ce6dd3e8f639cd26e0edfa3b912eddbda3
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-09-06 17:18:03 -0400

    Load hand-eye from file

--- simple_scene/zed_m_psm_ar_example.py
+++ simple_scene/zed_m_psm_ar_example.py
@@ -1,4 +1,7 @@
+import argparse
 from dataclasses import dataclass, field
+import json
+import pathlib
 from typing import List
 from ambf_client import Client
 import time
@@ -27,7 +30,7 @@ class PsmSub:
         return self.joint_state.copy()
 
 
-def setup():
+def setup(cam_opencv_T_base: np.ndarray):
 
     _client = Client()
     _client.connect()
@@ -37,18 +40,17 @@ def setup():
     time.sleep(0.2)
 
     # Set initial cam pose base on hand-eye calibration
-    set_camera_pose(camera)
+    set_camera_pose(camera, cam_opencv_T_base)
 
     return _client, psm, camera
 
 
-def set_camera_pose( camera_handle):
-    # fmt: off
-    # TODO load directly from hand-eye calibration opencv file
-    cam_opencv_T_base = [[-0.8790781792313629, 0.3412618733306592, -0.3328090873310377, -0.08238268273316497], 
-                         [0.33239054126068474, 0.9392881778897073, 0.08517186716905183, -0.007596158536450624], 
-                         [0.34166955216948763, -0.03574986276173252, -0.9391399599808413, 0.09819970244543609], 
-                         [0.0, 0.0, 0.0, 1.0]]
+def set_camera_pose(camera_handle, cam_opencv_T_base: np.ndarray):
+    # # fmt: off
+    # cam_opencv_T_base = [[-0.8790781792313629, 0.3412618733306592, -0.3328090873310377, -0.08238268273316497], 
+    #                      [0.33239054126068474, 0.9392881778897073, 0.08517186716905183, -0.007596158536450624], 
+    #                      [0.34166955216948763, -0.03574986276173252, -0.9391399599808413, 0.09819970244543609], 
+    #                      [0.0, 0.0, 0.0, 1.0]]
     # fmt: on
     cam_opencv_T_base = np.array(cam_opencv_T_base)
     base_T_cam_opencv = np.linalg.inv(cam_opencv_T_base) 
@@ -65,8 +67,33 @@ def virtual_psm_measured_jp(psm_base, joint_state):
     for i in range(6):
         psm_base.set_joint_pos(i, joint_state[i])
 
+def load_hand_eye_calibration(json_file: pathlib.Path) -> np.ndarray:
+
+    if not json_file.exists():
+        raise FileNotFoundError(f"Hand-eye calibration file not found: {json_file}")
+
+    with open(json_file, "r") as f:
+        data = json.load(f)
+
+    cam_T_robot_base = np.array(data['base-frame']['transform']).reshape(4, 4)
+
+    return cam_T_robot_base
+
 def main():
-    _client, virtual_psm, virtual_camera = setup()
+
+    parser = argparse.ArgumentParser()
+    # fmt: off
+    parser.add_argument(
+        "-H", "--hand-eye-json", type=str, required=True,
+        help="hand-eye calibration matrix in JSON format using OpenCV coordinate system",
+    )
+    # fmt: on
+    args = parser.parse_args()
+
+    hand_eye_path = pathlib.Path(args.hand_eye_json)
+    cam_opencv_T_base = load_hand_eye_calibration(hand_eye_path)
+
+    _client, virtual_psm, virtual_camera = setup(cam_opencv_T_base)
 
     psm_sub = PsmSub()
 
