c02b7af398a1251c5cada63a87afba3d5bc899be
Junxiang Wang <wangwang.0418@hotmail.com>
2024-11-08 12:51:54 -0500

    update communication loss generator

--- scripts/com_loss_generator.py
+++ scripts/com_loss_generator.py
@@ -1,65 +1,54 @@
 import rospy
 from std_msgs.msg import Bool
-import random
+from numpy import random
 import time
 
 def com_loss_publisher():
     pub = rospy.Publisher('communication_loss', Bool, queue_size=1)
+    pub_ar = rospy.Publisher('ar_activate', Bool, queue_size=1)
     rospy.init_node('communication_loss_talker', anonymous=True)
 
     i = 0
-    flag = False
+    comm_loss = False
 
-    time_period = 9
-    loss_period = 1
+    comm_good_mean = 1.2
+    comm_good_std = 0.15
+    comm_loss_mean = 0.6
+    comm_loss_std = 0.1
 
     hz = 200
     rate = rospy.Rate(hz) # 200hz
 
-    print("TIME_period: " + str(time_period) + " sec")
-    print("LOSS_period: "  + str(loss_period) + " sec")
+    comm_good_period = random.normal(comm_good_mean, comm_good_std)
+    comm_loss_period = random.normal(comm_loss_mean, comm_loss_std)
+
+    print("TIME_period: " + str(comm_good_period) + " sec")
+    print("LOSS_period: "  + str(comm_loss_period) + " sec")
+
+    pub.publish(comm_loss)
 
     while not rospy.is_shutdown():
         i += 1
 
-        if (i == int(time_period - loss_period) * hz):
-            flag = True
-            pub.publish(flag)
-
-        if (i == int(time_period * hz)):
-            flag = False
-            pub.publish(flag)
+        if ((not comm_loss) and i == int(comm_good_period * hz)):
+            comm_loss = True
+            pub.publish(comm_loss)
+            pub_ar.publish(comm_loss)
             i = 0
 
-            # Added for random generation loss
-            time_period = 10 * random.random()
-            loss_period = 5 * random.random()
-            while time_period <  loss_period:
-                time_period = loss_period +1 
+        if (comm_loss and i == int(comm_loss_period * hz)):
+            comm_loss = False
+            pub.publish(comm_loss)
+            pub_ar.publish(comm_loss)
+            i = 0
 
-            print("TIME_period: " + str(time_period) + " sec")
-            print("LOSS_period: "  + str(loss_period) + " sec")
+            comm_good_period = random.normal(comm_good_mean, comm_good_std)
+            comm_loss_period = random.normal(comm_loss_mean, comm_loss_std)
 
-        #pub.publish(flag)
+            print("TIME_period: " + str(comm_good_period) + " sec")
+            print("LOSS_period: "  + str(comm_loss_period) + " sec")
+
         rate.sleep()
-
-    # now = rospy.get_time()
-    # flag = False
-    # while not rospy.is_shutdown():
-    #
-    #     if (rospy.get_time() - now > 10):
-    #         flag = True
-    #
-    #     if (rospy.get_time() - now > 12):
-    #         flag = False
-    #         now = rospy.get_time()
-    #
-    #     pub.publish(flag)
-    #     rate.sleep()
-
-
-
-
 
 
 if __name__ == '__main__':
