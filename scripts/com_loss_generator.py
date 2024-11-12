5e9ea761c464351b2aed566226ce1b010ba7ce12
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-11-11 19:45:39 -0500

    Reformat comm loss generator

--- scripts/com_loss_generator.py
+++ scripts/com_loss_generator.py
@@ -3,40 +3,41 @@ from std_msgs.msg import Bool
 from numpy import random
 import time
 
+
 def com_loss_publisher():
-    pub = rospy.Publisher('communication_loss', Bool, queue_size=1)
-    pub_ar = rospy.Publisher('ar_activate', Bool, queue_size=1)
-    rospy.init_node('communication_loss_talker', anonymous=True)
+    pub = rospy.Publisher("communication_loss", Bool, queue_size=1)
+    pub_ar = rospy.Publisher("ar_activate", Bool, queue_size=1)
+    rospy.init_node("communication_loss_talker", anonymous=True)
 
     i = 0
     comm_loss = False
 
-    comm_good_mean = 3.2
-    comm_good_std = 0.15
-    comm_loss_mean = 0.8
+    comm_good_mean = 5.0
+    comm_good_std = 0.1
+    comm_loss_mean = 1.0
     comm_loss_std = 0.1
 
-    hz = 200
-    rate = rospy.Rate(hz) # 200hz
+    hz = 50
+    rate = rospy.Rate(hz)  # 200hz
 
     comm_good_period = random.normal(comm_good_mean, comm_good_std)
     comm_loss_period = random.normal(comm_loss_mean, comm_loss_std)
 
     print("TIME_period: " + str(comm_good_period) + " sec")
-    print("LOSS_period: "  + str(comm_loss_period) + " sec")
+    print("LOSS_period: " + str(comm_loss_period) + " sec")
 
     pub.publish(comm_loss)
 
     while not rospy.is_shutdown():
         i += 1
 
-        if ((not comm_loss) and i == int(comm_good_period * hz)):
+        if (not comm_loss) and i == int(comm_good_period * hz):
             comm_loss = True
             pub.publish(comm_loss)
             pub_ar.publish(comm_loss)
             i = 0
 
-        if (comm_loss and i == int(comm_loss_period * hz)):
+        if comm_loss and i == int(comm_loss_period * hz):
             comm_loss = False
             pub.publish(comm_loss)
             pub_ar.publish(comm_loss)
@@ -46,17 +47,17 @@ def com_loss_publisher():
             comm_loss_period = random.normal(comm_loss_mean, comm_loss_std)
 
             print("TIME_period: " + str(comm_good_period) + " sec")
-            print("LOSS_period: "  + str(comm_loss_period) + " sec")
+            print("LOSS_period: " + str(comm_loss_period) + " sec")
 
         rate.sleep()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("COMMUNICATION LOSS GENERATOR")
     # print("Please enter your seed:")
     # time.sleep(2)
     # num_seed = input(":\n")
-    
+
     # print(f'Seed Number: {num_seed}')
 
     # random.seed(num_seed)
